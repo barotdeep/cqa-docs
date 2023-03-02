f860e3ca65e732b4a4e284129e8dc4090ab569ea
Akash shah <shahakash277@gmail.com>
2023-03-02 17:17:48 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
 
 
   // Set the production url of your site here
-  url: 'https://barotdeep.github.io/',
+  url: 'https://barotdeep.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cqa-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
