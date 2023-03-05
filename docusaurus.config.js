ef4d7de5673268958b0b305895258857e029e645
Deep <barotdeep5@gmail.com>
2023-03-05 10:42:20 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'ContextQA',
-  tagline: 'Test with ContextQA',
+  tagline: 'AI-Driven Software Test Automation Platform',
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
